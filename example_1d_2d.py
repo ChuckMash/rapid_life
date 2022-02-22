0c7d6fd59f6925ab1ff6d4e2363673a1b2a65362
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2022-02-21 22:41:38 -0800

    Update example_1d_2d.py

--- example_1d_2d.py
+++ example_1d_2d.py
@@ -2,7 +2,7 @@ import rapid_life
 
 rl = rapid_life.rapid_life(fullscreen=True)
 
-rl.add_instruction()
-rl.add_instruction( rules=[20,1,5], neighborhood=[[0],[1]], anchor=[0,0])
+rl.add_instruction( rules=[2,3,4], neighborhood=[[1,1,1],[1,0,1],[1,1,1]], anchor=[1,1] )
+rl.add_instruction( rules=[20,1,5], neighborhood=[[0],[1]], anchor=[0,0] )
 
 rl.run()
