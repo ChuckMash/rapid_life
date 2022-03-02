765a3321320d175c52dad361b1d4a462fdd3bff7
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2022-03-01 21:25:48 -0800

    Update example_benchmark.py

--- example_benchmark.py
+++ example_benchmark.py
@@ -1,25 +1,39 @@
 import rapid_life
 import os
+import cv2
+import platform
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
+
+###
 run_for = 1000
+display = False
+use_umat = True
+###
+
 
 clock = pygame.time.Clock()
 fps_log = []
 
-life = rapid_life.rapid_life(res=(1920,1080), use_umat=True, fullscreen=True)
+life = rapid_life.rapid_life(res=(1920,1080), use_umat=use_umat, fullscreen=True)
 life.randomize_board()
 
 for i in range(run_for):
   life.step_forward()
-  #life.display_board()
+  if display:
+    life.display_board()
   clock.tick()
   fps = clock.get_fps()
 
   if fps:
     fps_log.append(fps)
 
+print("OpenCV:",      cv2.__version__)
+print(platform.platform())
+print(platform.processor())
+print("UMat:",        life.use_umat)
+print("display",      display)
+print("Steps:",       run_for)
 print("Average FPS:", sum(fps_log) / len(fps_log))
-
 
